aaa30cf64447ffdbf753d6bfef078ab82945290f
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-02-01 09:12:02 +0100

    Creado proyecto y dependencias

---
+++ src/test/java/com/mafv/springprojects/exament4/Exament4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafv.springprojects.exament4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exament4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
